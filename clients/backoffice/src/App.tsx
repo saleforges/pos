50e79ffd12aab17e6acfb5fe1f0b9df221ce8fab
ilham <ilhamksyuriadi@gmail.com>
2026-07-08 23:26:04 +0700

    chore: set up turborepo at clients

--- clients/backoffice/src/App.tsx
+++ clients/backoffice/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-gray-900">Backoffice test</h1>
+      <h1 className="text-3xl font-bold text-gray-900">Backoffice test with turbo</h1>
     </div>
   )
 }
