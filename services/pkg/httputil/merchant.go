a20ae9ced2fbce7bed6f6c6f5759904ff0817007
ilhampraset30 <ilhampraset30@gmail.com>
2026-07-13 01:46:06 +0700

    refactor(pkg): GetMerchantID returns int64 instead of string

--- services/pkg/httputil/merchant.go
+++ services/pkg/httputil/merchant.go
@@ -2,6 +2,7 @@ package httputil
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -14,30 +15,38 @@ const (
 func MerchantMiddleware() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			merchantID := c.Request().Header.Get("X-Merchant-Id")
+			merchantIDStr := c.Request().Header.Get("X-Merchant-Id")
 			userType, _ := c.Get(ContextKeyUserType).(string)
 
 			// Platform user: header optional
 			if userType == "platform" {
-				if merchantID != "" {
-					c.Set(ContextKeyMerchantID, merchantID)
+				if merchantIDStr != "" {
+					if id, err := strconv.ParseInt(merchantIDStr, 10, 64); err == nil {
+						c.Set(ContextKeyMerchantID, id)
+					}
 				}
 				return next(c)
 			}
 
 			// Merchant user or unknown: header required
-			if merchantID == "" {
+			if merchantIDStr == "" {
 				return c.JSON(http.StatusBadRequest, map[string]string{
 					"error": "X-Merchant-Id header is required",
 				})
 			}
-			c.Set(ContextKeyMerchantID, merchantID)
+			id, err := strconv.ParseInt(merchantIDStr, 10, 64)
+			if err != nil {
+				return c.JSON(http.StatusBadRequest, map[string]string{
+					"error": "invalid X-Merchant-Id header",
+				})
+			}
+			c.Set(ContextKeyMerchantID, id)
 			return next(c)
 		}
 	}
 }
 
-func GetMerchantID(c echo.Context) string {
-	id, _ := c.Get(ContextKeyMerchantID).(string)
+func GetMerchantID(c echo.Context) int64 {
+	id, _ := c.Get(ContextKeyMerchantID).(int64)
 	return id
 }
