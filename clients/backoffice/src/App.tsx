2997ad53c3ffbb2cfaf0e57a9b326ca50c07f9ac
ilham <ilhamksyuriadi@gmail.com>
2026-07-22 22:19:49 +0700

    trigger deploy backoffice

--- clients/backoffice/src/App.tsx
+++ clients/backoffice/src/App.tsx
@@ -2,7 +2,7 @@
 import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from '@/features/auth/context/AuthContext';
 import { router } from '@/router';
-
+// comment for trigger deploy
 function App() {
   return (
     <AuthProvider>
